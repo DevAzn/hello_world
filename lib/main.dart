79c38dacd30247c88e5e39c97cd38109742fc076
John Jennings <jjennings1125@gmail.com>
2019-05-30 20:40:53 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+// This is a test
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
